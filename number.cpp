15dd3f96b7b297b105aa36fd8ce4c27f57e87845
tetianamart <117523470+tetianamart@users.noreply.github.com>
2022-11-10 22:57:38 +0200

    Update number.cpp

--- number.cpp
+++ number.cpp
@@ -1,18 +1,1 @@
-#include <iostream>
-#include <cmath>
 
-
-int main()
-{
-    int n, i;
-    int suma=0;
-    printf( "Number: ");
-    std::cin >> n;
-    while ( n > 0) {
-        i = n % 10;
-        suma+=pow(i,2);
-        n /= 10;
-    }
-    std::cout << suma ;
-    return 0;
-}
